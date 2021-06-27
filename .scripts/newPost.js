a044a81c79bbdd462d689b7ed3d8c4198c8dc69a
Alexander Morgunov <saaaaaaaaasha@yandex-team.ru>
2021-06-27 18:52:16 +0700

    New post "Typescript challenge: flatArray"

--- .scripts/newPost.js
+++ .scripts/newPost.js
@@ -1,49 +1,67 @@
 const fs = require('fs');
-const { DateTime } = require('luxon');
+const {DateTime} = require('luxon');
+const {argv} = require('yargs');
+const {spawnSync} = require('child_process');
+
+const help = `Usage: node ./scripts/newPost <options>
+
+options:
+    --title=<title>  Required post title
+    --slug=<slug>    Post slug
+    --date=<date>    Post published date in YYYY-MM-DD
+`;
+
+if (argv.help || argv.h) {
+	console.log(help);
+	process.exit(0);
+}
+
+if (!argv.title) {
+  throw new Error(
+    'The title is required.\n' +
+    'You should run script like `node ./scripts/newPost --title=<title>`.'
+  );
+}
+
+const formattedSlug = argv.slug || argv.title
+  .toLowerCase()
+  .replace(/[^a-z0-9а-яё]/g, '-');
+
+const date = argv.date
+  ? DateTime.fromISO(argv.date)
+  : DateTime.fromJSDate(new Date(), { zone: 'utc' })
+
+const formattedDate = date.toFormat('yyyy-LL-dd');
 
 const template = `---
 title: "{title}"
 date: {date}
 time: 5
 description: ""
+featuredImageThumbnail: "/assets/images/{date}-{slug}/preview.jpg"
 tags:
   - {tagName}
 layout: layouts/post.njk
 likes: 0
 ---`;
 
-const [, , title, slug, argDate] = process.argv;
-
-if (!title) {
-  throw new Error(
-    'The title is required.\n' +
-    'You should run script like `npm run create-post <title>`.'
-  );
-}
-
-const formattedSlug = slug || title
-  .toLowerCase()
-  .replace(/[^a-z0-9а-яё]/g, '-');
-
-const date = argDate
-  ? DateTime.fromISO(argDate)
-  : DateTime.fromJSDate(new Date(), { zone: 'utc' })
-
-const formattedDate = date.toFormat('yyyy-LL-dd');
-
 const fileName = `${formattedDate}-${formattedSlug}`;
 const path = `./src/posts/${fileName}.md`;
 const content = template
-  .replace('{title}', title)
-  .replace('{date}', formattedDate);
+  .replace(/{title}/g, argv.title)
+  .replace(/{slug}/g, formattedSlug)
+  .replace(/{date}/g, formattedDate);
 
-fs.mkdirSync(`./src/assets/images/${fileName}`);
-fs.writeFileSync(path, content, err => {
-  if (err) {
-    console.log(err);
+if (!fs.existsSync(`./src/assets/images/${fileName}`)) {
+  fs.mkdirSync(`./src/assets/images/${fileName}`);
+}
 
-    return;
-  }
+if (!fs.existsSync(path)) {
+  fs.writeFileSync(path, content);
+}
 
-  console.log(`Post was created in ${path}!`);
-});
+console.log(`Post was created in ${path}`)
+
+console.log(`Next run command: node .scripts/generatePreview --slug=${fileName}`);
+
+// spawnSync(`node ./generatePreview --slug=${fileName}`);
