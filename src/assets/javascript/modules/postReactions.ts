32b0777d44239d32f386bb1adacacb91d88fdeb6
amorgunov <amorgunov@samokat.ru>
2022-05-02 10:09:10 +0300

    Update post reactions

--- src/assets/javascript/modules/postReactions.ts
+++ src/assets/javascript/modules/postReactions.ts
@@ -1,6 +1,13 @@
-interface IReactions {
-  [key: string]: number;
-}
+type ReactionCode = 'shocked' | 'love' | 'like' | 'dislike' | 'rage';
+type Reactions = Record<ReactionCode, number>;
+
+const EMOJI_MAP: Record<ReactionCode, string> = {
+  shocked: '🤯',
+  love: '😍',
+  like: '👍',
+  dislike: '👎',
+  rage: '😡',
+};
 
 const REACTION_ENDPOINT =
   'https://i7on6ck7ng.execute-api.us-east-2.amazonaws.com/dev/posts/{slug}/likes';
@@ -11,17 +18,22 @@ function getSlug() {
   return slug;
 }
 
-function renderItems(reactions: IReactions, postReactionWrapperEl: HTMLDivElement) {
+function getEmojiFromCode(code: ReactionCode) {
+  return EMOJI_MAP[code];
+}
+
+function renderItems(reactions: Reactions, postReactionWrapperEl: HTMLDivElement) {
   if (typeof reactions !== 'object') {
     return;
   }
 
   const content = Object.entries(reactions).map(([type, count]) => {
     return `
-      <div data-type="${type}" class="post_emotion post_emotion_type_${type}">
-        <div class="post_emotion__counter ${count ? '' : 'post_emotion__counter_empty'}">
-          ${count}
-        </div>
+      <div class="tags__item">
+        <a data-type="${type}" href="#" class="tags__item-link">
+          <span style="font-size: 20px;">${getEmojiFromCode(type as ReactionCode)}</span>
+          <span style="padding-left: 5px;">${count}</span>
+        </a>
       </div>
     `;
   });
@@ -36,17 +48,26 @@ function getReaction(postReactionWrapperEl: HTMLDivElement) {
     .catch(err => console.error(err));
 }
 
-function setReaction(type: string, postReactionWrapperEl: HTMLDivElement) {
+function setReaction(type: string, postReactionWrapperEl: HTMLDivElement): Promise<boolean> {
   const params = {
     method: 'POST',
     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
     body: `{"reactionId":"${type}"}`, // , "testSourceIp": "192.168.0.1"}`
   };
 
-  fetch(REACTION_ENDPOINT.replace('{slug}', getSlug()), params)
+  return fetch(REACTION_ENDPOINT.replace('{slug}', getSlug()), params)
     .then(response => response.json())
-    .then(result => !result.error && renderItems(result, postReactionWrapperEl))
-    .catch(err => console.error(err));
+    .then(result => {
+      if (result.error) {
+        return true;
+      }
+
+      renderItems(result, postReactionWrapperEl);
+    })
+    .catch(err => {
+      console.error(err);
+      return true;
+    });
 }
 
 export function initPostReactions() {
@@ -56,15 +77,30 @@ export function initPostReactions() {
     return;
   }
 
+  postReactionWrapperEl.classList.remove('post__emotions_error');
+
   getReaction(postReactionWrapperEl);
 
   postReactionWrapperEl.addEventListener('click', (event: MouseEvent) => {
     const {target} = event;
 
-    if (!target || !(target as HTMLDivElement).dataset.type) {
+    event.preventDefault();
+
+    if (!target) {
       return;
     }
 
-    setReaction((target as HTMLDivElement).dataset.type, postReactionWrapperEl);
+    const typedEl: HTMLLinkElement =
+      (target as HTMLLinkElement).closest('.tags__item-link') || (target as HTMLLinkElement);
+
+    if (!typedEl.dataset.type) {
+      return;
+    }
+
+    setReaction(typedEl.dataset.type, postReactionWrapperEl).then(hasError => {
+      if (hasError) {
+        postReactionWrapperEl.classList.add('post__emotions_error');
+      }
+    });
   });
 }
