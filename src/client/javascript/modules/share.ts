86b59a669f6da81640bc7128f9eda9f76ddabc57
Alexander <noveogroupamorgunov@gmail.com>
2022-10-30 15:48:09 +0300

    feat: Share button (#18)
    * feat: Share button
    
    * fixes

---
+++ src/client/javascript/modules/share.ts
@@ -0,0 +1,34 @@
+export function isWebShareSupported() {
+  if (!('share' in window.navigator)) {
+    return false;
+  }
+
+  if ('canShare' in navigator) {
+    const url = `https://${window.location.hostname}`;
+    return (window.navigator as any).canShare({url});
+  }
+
+  return true;
+}
+
+export function registerShare() {
+  const button = document.querySelector('[data-id=share-button') as HTMLButtonElement;
+
+  if (!button || !isWebShareSupported()) {
+    return;
+  }
+
+  const shareData = {
+    text: document.title,
+    url: window.location.href,
+  };
+
+  button.addEventListener('click', (event: MouseEvent) => {
+    try {
+      event.preventDefault();
+      navigator.share(shareData);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+}
